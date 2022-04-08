2f14a7462792a9b197c9b5a0e0d5ae0c78acae05
abhishek ayithe <abhishek.ayithe@c2info.com>
2022-04-08 19:20:28 +0530

    use for data clean efficient

--- src/main/kotlin/SingleQuotesMain.kt
+++ src/main/kotlin/SingleQuotesMain.kt
@@ -6,8 +6,18 @@ fun main(){
 }
 class SingleQuotesMain {
     fun getData(){
-        var data=File("C:\\Users\\CSQ\\IdeaProjects\\SingleQuotes\\src\\main\\resources\\input.txt").readText()
-        data=data.replace("\n","\',\'")
-        File("C:\\Users\\CSQ\\IdeaProjects\\SingleQuotes\\src\\main\\resources\\output.txt").writeText("'$data'")
+        val filein = File("C:\\Users\\CSQ\\IdeaProjects\\SingleQuotes\\src\\main\\resources\\input.txt")
+        val fileout = File("C:\\Users\\CSQ\\IdeaProjects\\SingleQuotes\\src\\main\\resources\\output.txt")
+
+        var data = filein.readLines()
+        var totalData=""
+        for(i in 0..data.size){
+            totalData+=data[i]
+            if(i==data.size-1){
+                break
+            }
+            totalData+="\',\'"
+        }
+        fileout.writeText("'$totalData'")
     }
 }
